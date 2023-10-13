976718de9c1f94a94bb3593e6be20b7ec321b048
Natali Ustinova <nata@purple.studio>
2023-10-13 23:41:09 +0300

    feat: Initial file structure

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './app-header.module.css';
+
+const AppHeader = () => (
+  <header className={styles.container}>
+  </header>
+);
+
+export default AppHeader;
---
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './burger-ingredients.module.css';
+
+const BurgerIngredients = () => (
+  <section className={styles.container}>
+  </div>
+);
+
+export default BurgerIngredients;
