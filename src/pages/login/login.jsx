0a8399910ef16c8a935ed099c474c330f85f4d4f
Natali Ustinova <nata@purple.studio>
2023-11-20 22:26:11 +0200

    feat: login and register layout

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,10 +1,65 @@
 import React from "react";
 import styles from "./login.module.css";
+import { Link } from "react-router-dom";
+import {
+  Button,
+  Input,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 
 const Login = () => {
+  const [value, setValue] = React.useState("");
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
   return (
-    <div className={styles.loginContainer}>
-      <p>Страница авторизации</p>
+    <div className={styles.container}>
+      <h1 className="text text_type_main-medium">Вход</h1>
+      <Input
+        type="email"
+        placeholder="Email"
+        value={value}
+        onChange={(e) => handleChange(e)}
+        name={"password"}
+        error={false}
+        errorText={"Ошибка"}
+        size={"default"}
+        extraClass="mt-6 mb-6"
+      />
+      <Input
+        type="password"
+        placeholder="Пароль"
+        value={value}
+        onChange={(e) => handleChange(e)}
+        icon={"ShowIcon"}
+        onIconClick={() => {}}
+        name={"password"}
+        error={false}
+        errorText={"Ошибка"}
+        size={"default"}
+        extraClass="mb-6"
+      />
+      <Button type="primary" size="medium" htmlType="submit">
+        Войти
+      </Button>
+
+      <p className="mt-20 text_type_main-default text_color_inactive">
+        Вы — новый пользователь?
+        <Link
+          to="/register"
+          className={`${styles.link} ml-2 text_type_main-default text_color_accent`}
+        >
+          Зарегистрироваться
+        </Link>
+      </p>
+      <p className="mt-4 text_type_main-default text_color_inactive">
+        Забыли пароль?
+        <Link
+          to="/forgot-password"
+          className={`${styles.link} ml-2 text_type_main-default text_color_accent`}
+        >
+          Восстановить пароль
+        </Link>
+      </p>
     </div>
   );
 };
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,10 +1,67 @@
 import React from "react";
 import styles from "./register.module.css";
+import { Link } from "react-router-dom";
+import {
+  Button,
+  Input,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 
 const Register = () => {
+  const [value, setValue] = React.useState("");
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
   return (
-    <div className={styles.registerContainer}>
-      <p>Страница регистрации</p>
+    <div className={styles.container}>
+      <h1 className="text text_type_main-medium">Регистрация</h1>
+      <Input
+        type="name"
+        placeholder="Имя"
+        value={value}
+        onChange={(e) => handleChange(e)}
+        name={"name"}
+        error={false}
+        errorText={"Ошибка"}
+        size={"default"}
+        extraClass="mt-6 mb-6"
+      />
+      <Input
+        type="email"
+        placeholder="Email"
+        value={value}
+        onChange={(e) => handleChange(e)}
+        name={"password"}
+        error={false}
+        errorText={"Ошибка"}
+        size={"default"}
+        extraClass="mb-6"
+      />
+      <Input
+        type="password"
+        placeholder="Пароль"
+        value={value}
+        onChange={(e) => handleChange(e)}
+        icon={"ShowIcon"}
+        onIconClick={() => {}}
+        name={"password"}
+        error={false}
+        errorText={"Ошибка"}
+        size={"default"}
+        extraClass="mb-6"
+      />
+      <Button type="primary" size="medium" htmlType="submit">
+        Зарегистрироваться
+      </Button>
+
+      <p className="mt-20 text_type_main-default text_color_inactive">
+        Уже зарегистрированы?
+        <Link
+          to="/login"
+          className={`${styles.link} ml-2 text_type_main-default text_color_accent`}
+        >
+          Войти
+        </Link>
+      </p>
     </div>
   );
 };
